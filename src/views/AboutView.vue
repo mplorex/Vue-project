3b760630f735c178de1b1c420fb4adc2dd3ff6fc
michael posselt <michaelposselt@outlook.com>
2022-07-18 12:54:46 -0500

    adding a counter

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,36 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <div class="counter">
+      {{count}}
+    </div>
+    <div class="buttons">
+      <button @click="decreaseCount">-</button>
+      <button @click="increaseCount">+</button>
+    </div>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+
+const increaseCount = () => {
+  count.value++
+}
+const decreaseCount = () => {
+  count.value--
+}
+</script>
+
+<style>
+.counter {
+  font-size: 30px;
+  margin: 20px;
+}
+.buttons button {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
